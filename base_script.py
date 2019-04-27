dadffa7d35fabfb7ed6b226c02bfd0ccba6cf6e7
root <root@DESKTOP-CCANCOE.localdomain>
2019-04-27 06:43:36 +0000

    Changes values to int to avoid fluctuations

--- base_script.py
+++ base_script.py
@@ -13,6 +13,7 @@ while True:
     tmp_print = sct.grab(sct.monitors[0])
 
     mean = numpy.reshape(tmp_print.pixels,(tmp_print.width*tmp_print.height,3)).mean(axis=0)
-    bulb.set_rgb(mean[0],mean[1],mean[2])
+    print(int(mean[0]),int(mean[1]),int(mean[2]))
+    bulb.set_rgb(int(mean[0]),int(mean[1]),int(mean[2]))
     elapsed_time = time.time() - start_time
     print(elapsed_time)
