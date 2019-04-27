1adecd3a43c092368e0532754db0a415d4da28bd
root <root@DESKTOP-CCANCOE.localdomain>
2019-04-27 06:59:05 +0000

    MUCH faster now

--- base_script.py
+++ base_script.py
@@ -3,6 +3,8 @@ import numpy
 import mss
 from yeelight import Bulb
 
+mon = {"top": 40, "left": 0, "width": 800, "height": 640}
+
 sct = mss.mss()
 
 bulb = Bulb("192.168.0.186")
@@ -10,10 +12,12 @@ bulb = Bulb("192.168.0.186")
 while True:
     start_time = time.time()
 
-    tmp_print = sct.grab(sct.monitors[0])
-
-    mean = numpy.reshape(tmp_print.pixels,(tmp_print.width*tmp_print.height,3)).mean(axis=0)
-    print(int(mean[0]),int(mean[1]),int(mean[2]))
-    bulb.set_rgb(int(mean[0]),int(mean[1]),int(mean[2]))
+    mean = numpy.asarray(sct.grab(mon)).reshape(512000,4).mean(axis=0)
+    print(int(mean[2]),int(mean[1]),int(mean[0]))
+    try:
+        bulb.set_rgb(int(mean[2]),int(mean[1]),int(mean[0]))
+    except Exception as e:
+        print(str(e))
     elapsed_time = time.time() - start_time
     print(elapsed_time)
+    time.sleep(0.5)
