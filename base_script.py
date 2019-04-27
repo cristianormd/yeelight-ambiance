5d629678a8ed5e329f485ca8fcb9965b697a39b7
root <root@DESKTOP-CCANCOE.localdomain>
2019-04-27 22:35:44 +0000

    Adds discovery and rediscovery of bulbs.

--- base_script.py
+++ base_script.py
@@ -1,13 +1,27 @@
 import time 
 import numpy
 import mss
+import yeelight
 from yeelight import Bulb
 
+ERROR_STR = "No Bulb available. Please make sure they are connected. If the issue persists, turn them off and on, and wait 5 seconds before attempting to run this program again."
+
+def assert_connect_bulbs():
+    infos = yeelight.discover_bulbs()
+
+    bulbs = []
+
+    for info in infos:
+        bulbs.append(yeelight.Bulb(info['ip']))
+    if(len(bulbs) == 0):
+        raise yeelight.BulbException(ERROR_STR)
+
+    return bulbs
+
+bulbs = assert_connect_bulbs()
+
 mon = {"top": 40, "left": 0, "width": 800, "height": 640}
-
 sct = mss.mss()
-
-bulb = Bulb("192.168.0.186")
 
 while True:
     start_time = time.time()
@@ -15,9 +29,12 @@ while True:
     mean = numpy.asarray(sct.grab(mon)).reshape(512000,4).mean(axis=0)
     print(int(mean[2]),int(mean[1]),int(mean[0]))
     try:
-        bulb.set_rgb(int(mean[2]),int(mean[1]),int(mean[0]))
+        for bulb in bulbs:
+            bulb.set_rgb(int(mean[2]),int(mean[1]),int(mean[0]))
     except Exception as e:
         print(str(e))
+        bulbs = assert_connect_bulbs()
+
     elapsed_time = time.time() - start_time
     print(elapsed_time)
     time.sleep(0.5)
